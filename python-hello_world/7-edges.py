0c853d6f99cb53bdca87f6d7e3ab0109e341c450
max2doherty <max.doherty@sportsbet.com.au>
2025-05-30 17:27:19 +1000

    Completed Task 5: Copy Cut Paste

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(f"First 3 letters: {word[:3]}")
+# first three letters
+print(f"Last 2 letters: {word[7:9]}")
+# last two letters
+print(f"Middle word: {word[1:8]}")
+# without first and last letter
