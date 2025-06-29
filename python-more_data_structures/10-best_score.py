da79d49b5a7334cc149f4463bed5f24ec822eb8a
max2doherty <max.doherty@sportsbet.com.au>
2025-06-29 19:18:17 +1000

    Completed Task 10: Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is  None or len(a_dictionary) == 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     top_scorer = None
     top_score = 0
