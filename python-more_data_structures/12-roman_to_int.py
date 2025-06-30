924fc422a414922399b8569c51107046a8d45e65
max2doherty <max.doherty@sportsbet.com.au>
2025-06-30 13:59:30 +1000

    Completed Task 12: Roman to integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,15 @@
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
-    numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    numerals = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
     integer = 0
     for i in range(len(roman_string)):
         if roman_string[i] not in numerals:
