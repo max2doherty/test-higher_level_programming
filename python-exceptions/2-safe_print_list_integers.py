f525069129d5f20a60d14d5508b82807b1d3c82a
max2doherty <max.doherty@sportsbet.com.au>
2025-07-04 18:00:23 +1000

    Completed Task 2: Print and count integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(x):
-        try: 
+    try:
+        for i in range(x): 
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end='')
                 count += 1
-        except(ValueError, TypeError):
-            break
+    except(ValueError, TypeError):
+        pass
     print()
     return count
