60ab5c9ebaad3f5d797cecfae3def701aa9fc990
max2doherty <max.doherty@sportsbet.com.au>
2025-07-25 17:40:31 +1000

    Completed Task 0: Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,6 +4,11 @@ def add_integer(a, b=98):
         add_integer function adds two numbers
         args a: an int or a float
             b: an int or a float
+        Returns:
+            int: The sum of a and b, both cast to integers.
+
+         Raises:
+             TypeError: If a or b are not integers or floats.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
