022525742e62ee35ae69c6d6bb351237b43e98af
max2doherty <max.doherty@sportsbet.com.au>
2025-07-30 18:32:16 +1000

    Completed Task 2: Say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""write a function that prints My Name is <first name> <last name>"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints My Name is 
+        arguments
+            first_name: a persons first name
+            last_name: a persons last name
+        return: My Name is <first_name> <last_name>
+        """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name).strip())
