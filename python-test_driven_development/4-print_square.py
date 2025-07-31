9db0d539a59850ceb9185639940dcbc9457526c0
max2doherty <max.doherty@sportsbet.com.au>
2025-07-31 16:30:17 +1000

    Completed task 3: Print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""write a function that prints a square with the character #"""
+def print_square(size):
+    """prints a square
+        arguments
+            size: the size of the square
+        return: a square of size size is printed
+        """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
