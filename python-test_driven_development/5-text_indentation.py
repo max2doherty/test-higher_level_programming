85b3a99da37bd0f0b19c95bf1e1482ca1bc4ee2f
max2doherty <max.doherty@sportsbet.com.au>
2025-07-31 17:06:36 +1000

    Completed Task 4: Text Indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,8 +6,7 @@
 
 def text_indentation(text):
     """Prints a text with 2 new lines after 
-        each of these characters: ., ? and :
-    """
+        each of these characters: ., ? and :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     
