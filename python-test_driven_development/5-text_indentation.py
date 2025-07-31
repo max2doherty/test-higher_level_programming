7d64e09b9500352829fa9173a2b814abd1698ece
max2doherty <max.doherty@sportsbet.com.au>
2025-07-31 17:05:28 +1000

    Task 4 test file

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""write a function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+"""write a function that prints a text with 
+    2 new lines after each of these characters: ., ? and :
+"""
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after each of these characters: ., ? and :"""
+    """Prints a text with 2 new lines after 
+        each of these characters: ., ? and :
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     
